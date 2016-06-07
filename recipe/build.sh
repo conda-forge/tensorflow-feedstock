6781a849733c9802f00f52b6490edebb7709a56a
Daniel Rodriguez <df.rodriguez143@gmail.com>
2016-06-07 15:34:45 -0700

    Update to 0.9.0rc0

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,16 +4,19 @@
 
 if [ `uname` == Darwin ]; then
     if [ "$PY_VER" == "2.7" ]; then
-        pip install --no-deps https://storage.googleapis.com/tensorflow/mac/tensorflow-0.8.0-py2-none-any.whl
+        pip install --no-deps https://storage.googleapis.com/tensorflow/mac/tensorflow-0.9.0rc0-py2-none-any.whl
     else
-        pip install --no-deps https://storage.googleapis.com/tensorflow/mac/tensorflow-0.8.0-py3-none-any.whl
+        pip install --no-deps https://storage.googleapis.com/tensorflow/mac/tensorflow-0.9.0rc0-py3-none-any.whl
     fi
 fi
 
 if [ `uname` == Linux ]; then
     if [ "$PY_VER" == "2.7" ]; then
-        pip install --no-deps https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.8.0-cp27-none-linux_x86_64.whl
-    else
-        pip install --no-deps https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.8.0-cp34-cp34m-linux_x86_64.whl
+        pip install --no-deps https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.9.0rc0-cp27-none-linux_x86_64.whl
+    elif [ "$PY_VER" == "3.4" ]; then
+        pip install --no-deps https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.9.0rc0-cp34-cp34m-linux_x86_64.whl
+    elif [ "$PY_VER" == "3.5" ]; then
+        pip install --no-deps https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.9.0rc0-cp35-cp35m-linux_x86_64.whl
+
     fi
 fi
