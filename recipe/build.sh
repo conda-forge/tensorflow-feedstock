b1567229f39cbd7c163943742aea4b81090ed9e8
H. Vetinari <h.vetinari@gmx.com>
2025-02-01 13:19:34 +1100

    define explicit --configs

--- recipe/build.sh
+++ recipe/build.sh
@@ -182,7 +182,7 @@ if [[ "${target_platform}" == "osx-arm64" ]]; then
   # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
   export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 elif [[ "${target_platform}" == "osx-64" ]]; then
-  echo "build --config=darwin_x86_64" >> .bazelrc
+  echo "build --config=macos_x86_64" >> .bazelrc
 fi
 export TF_ENABLE_XLA=1
 export BUILD_TARGET="//tensorflow/tools/pip_package:wheel //tensorflow/tools/lib_package:libtensorflow //tensorflow:libtensorflow_cc${SHLIB_EXT}"
